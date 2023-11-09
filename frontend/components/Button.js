72cb4a7937900751ce32eed4962477fe8d4d058b
Youth Innovations <info.youthinno@gmail.com>
2023-11-09 20:42:29 +0100

    Create Button.js

---
+++ frontend/components/Button.js
@@ -0,0 +1,12 @@
+// frontend/components/Button.js
+import React from "react";
+
+const Button = ({ children, onClick }) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
